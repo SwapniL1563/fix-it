24cb65dd4d4415aacfd3670256ef4a283ed3a971
SwapniL1563 <choratswapnil15@gmail.com>
2025-08-08 22:55:34 +0530

    feat:changes before deployment

--- app/payment/cancel/page.tsx
+++ app/payment/cancel/page.tsx
@@ -1,6 +1,8 @@
-"use client";
+'use client';
 
 import { useSearchParams, useRouter } from "next/navigation";
+
+export const dynamic = "force-dynamic";
 
 export default function PaymentCancelPage() {
   const searchParams = useSearchParams();
