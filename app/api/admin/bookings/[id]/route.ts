91f40b645fbb663ff8207fce7bc95e9ebb7b6478
SwapniL1563 <choratswapnil15@gmail.com>
2025-08-08 13:08:18 +0530

    feat:changes before deployment

--- app/api/admin/bookings/[id]/route.ts
+++ app/api/admin/bookings/[id]/route.ts
@@ -1,30 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export async function PATCH(req:NextRequest,{params}:{params:{id:string}}) {
-    const { status } = await req.json();
+export async function PATCH(req: NextRequest,context: { params: { id: string } }) {
+  const { id } = context.params;
+  const { status } = await req.json();
 
-    try {
-        const booking =  await prisma.booking.update({
-            where:{id:params.id},
-            data: { status}
-        });
+  try {
+    const booking = await prisma.booking.update({
+      where: { id },
+      data: { status },
+    });
 
-        return NextResponse.json(booking)
-    } catch(error) {
-        return NextResponse.json({ error :"Failed to update booking"}, {status:500});
-    }
+    return NextResponse.json(booking);
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to update booking" }, { status: 500 });
+  }
 }
 
+export async function DELETE( req: NextRequest,context: { params: { id: string } }) {
+  const { id } = context.params;
 
-export async function DELETE(req:NextRequest,{ params}:{params:{id:string}}) {
-    try {
-        await prisma.booking.delete({
-            where:{id:params.id}
-        });
+  try {
+    await prisma.booking.delete({
+      where: { id },
+    });
 
-        return NextResponse.json({ message:"Deleted booking successfully"} , { status : 200})
-   } catch(error) {
-        return NextResponse.json({ error :"Failed to delete booking"}, {status:500});
-    }
+    return NextResponse.json({ message: "Deleted booking successfully" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to delete booking" }, { status: 500 });
+  }
 }
