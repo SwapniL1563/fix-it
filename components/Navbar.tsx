ea9e9077d7157cf6a4c58f43e0f0bb49f0ab25ae
SwapniL1563 <choratswapnil15@gmail.com>
2025-08-17 17:34:42 +0530

    feat:update navbar btn

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -56,7 +56,7 @@ export function NavbarDemo() {
                 setIsMobileMenuOpen(false);
                 router.push("/api/auth/signin");
               }}
-              variant="secondary"
+              variant="custom"
             >
               Login
             </NavbarButton>
@@ -65,7 +65,7 @@ export function NavbarDemo() {
                 setIsMobileMenuOpen(false);
                 router.push("/signup");
               }}
-              variant="secondary"
+              variant="custom"
             >
               Signup
             </NavbarButton>
